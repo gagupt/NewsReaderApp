a70803dec569545eab6fa4ea99b3d325a2d2eec2
Gaurav Gupta <gaurav.g@glowroad.com>
2020-04-19 09:09:13 +0530

    Updates

---
+++ precache-manifest.9411b81cbe6c8ef085316980f5817aae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "94c8c969c8792e44aba2",
+    "url": "/NewsReaderApp/static/js/main.94c8c969.chunk.js"
+  },
+  {
+    "revision": "b57ae6c4a3c69aa606a0",
+    "url": "/NewsReaderApp/static/js/1.b57ae6c4.chunk.js"
+  },
+  {
+    "revision": "cee1c5ef64d510a56bc3",
+    "url": "/NewsReaderApp/static/js/runtime~main.cee1c5ef.js"
+  },
+  {
+    "revision": "c9df0a30d2c7c93039cc98c7578da034",
+    "url": "/NewsReaderApp/index.html"
+  }
+];
